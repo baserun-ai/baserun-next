68d7e6f03d2f193d0421d13fb88cbc135f877c64
Tim Suchanek <tim.suchanek@gmail.com>
2023-12-01 16:35:17 -0800

    use next canary for reproduction

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,8 @@
 export async function register() {
   // Baserun is not yet supported in the edge runtime
   if (
-    process.env.NODE_ENV === 'production' &&
+    // for test purposes commented out, but you would want to uncomment this
+    // process.env.NODE_ENV === 'production' &&
     process.env.NEXT_RUNTIME === 'nodejs'
   ) {
     const { baserun } = await import('baserun')
