47413a631f6a96cf86a6eb8c2c04add8a906f836
Tim Suchanek <tim.suchanek@gmail.com>
2023-12-01 16:17:49 -0800

    only log in production

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,9 +1,7 @@
-// import { baserun } from 'baserun'
-
 export async function register() {
   // Baserun is not yet supported in the edge runtime
   if (
-    // process.env.NODE_ENV === 'production' &&
+    process.env.NODE_ENV === 'production' &&
     process.env.NEXT_RUNTIME === 'nodejs'
   ) {
     const { baserun } = await import('baserun')
